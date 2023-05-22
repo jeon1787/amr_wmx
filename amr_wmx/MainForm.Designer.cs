54c702fb1580639302fb8f2f9f2dfb73bce6a217
ewjeon <wjsdmsdn1787@naver.com>
2023-05-23 02:39:10 +0900

    add apis

--- amr_wmx/MainForm.Designer.cs
+++ amr_wmx/MainForm.Designer.cs
@@ -28,13 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(110, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(690, 380);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "WMX\r\nCommunication\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Servo\r\nControl";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
---
+++ amr_wmx/WMXController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMX3ApiCLR;
+
+namespace amr_wmx
+{
+    public class WMXController
+    {
+        private static WMXController controller;
+        private static WMX3Api device;
+        private static CoreMotion coreMotion;
+        int apiCallError;
+        public WMX3Api WMX3API
+        {
+            get { return device; }
+            set { device = value; }
+        }
+        public static WMXController GetInstance()
+        {
+            if (null == controller)
+            {
+                controller = new WMXController();
+            }
+
+            return controller;
+        }
+
+        #region initialize
+        public WMXController()
+        {       
+            device = new WMX3Api();
+            coreMotion = new CoreMotion();
+        }
+        #endregion
+
+        #region CreateDevice
+        public void CreateDevice()
+        {
+            if (null != device)
+            {
+                apiCallError = device.CreateDevice("C:\\Program Files\\SoftServo\\WMX3\\", DeviceType.DeviceTypeNormal, 1000);
+                if (ErrorCode.SystemInitializationNotDone == apiCallError)
+                {
+                    // Error
+                    Console.WriteLine(string.Format("[CreateDevice] Device is created successfully but initialization of system is not done in specified time."));
+                }
+                device.SetDeviceName("device_01");
+            }
+        }
+        #endregion
+
+        #region Start Comm
+        public void StartCommunication()
+        {
+            if (null != device)
+            {
+                apiCallError = device.StartCommunication(1000);
+                if (ErrorCode.StartCommunicationError == apiCallError) 
+                {
+                    // Error
+                    Console.WriteLine("[StartCommunication] Start communication failed. Please check the connection of the slave.");
+                } 
+            }
+        }
+        #endregion
+
+        #region Stop Comm
+        public void StopCommunication()
+        {
+            if(null != device)
+            {
+                // 성공적으로 연결 종료시 Engine State : Communicating -> Running
+                device.StopCommunication(1000);
+            }
+        }
+        #endregion
+
+        #region Start Engine
+        public void StartEngine()
+        {
+            if (null != device)
+            {
+
+                DevicesInfo devInfo = new DevicesInfo();
+                device.GetAllDevices(ref devInfo);
+                if (0 == devInfo.Count)
+                {
+                    // Start Engine : Automatically start engine on CreateDevice().
+                    CreateDevice();
+                }
+                else
+                {
+                    EngineStatus status = new EngineStatus();
+                    device.GetEngineStatus(ref status);
+                    if (EngineState.Running == status.State)
+                    {
+                        // Restart Engine
+                        device.RestartEngine("C:\\Program Files\\SoftServo\\WMX3\\", 1000);
+                    }
+                }
+
+            }
+        }
+        #endregion
+
+        #region Stop Engine
+        public void StopEngine()
+        {
+            if (null != device)
+            {
+                device.StopEngine();
+            }
+        }
+        #endregion
+
+        #region Set Servo ON
+        public void SetServoOn(int axis, bool turnOn) {
+            if (null != device)
+            {
+                if (turnOn)
+                {
+                    // ON
+                    coreMotion.AxisControl.SetServoOn(0, 1);
+                }
+                else
+                {
+                    // OFF
+                    coreMotion.AxisControl.SetServoOn(0, 0);
+                }
+
+            }
+        }
+        #endregion
+    }
+}
